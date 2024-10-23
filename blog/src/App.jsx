289f45f1c5dab7d9aaabb1b483614cc74881a16e
nlina-0 <ngolina0@gmail.com>
2024-10-23 14:02:46 +1100

    re-organised

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,25 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import CoinBase from './CoinBase'
-import CurrencySelector from './CurrencySelector'
 import Currency from './Currency'
 
 function App() {
-  const [currency, setCurrency] = useState("AUD")
-
-  // Abstract away setCurrency from CurrencySelector component, so it doesn't know what setCurrency is
-  const updateCurrency = (code) => {
-    // Validation of code
-    setCurrency(code)
-  }
+  
 
   return (
     <>
       <Currency />
-      <CoinBase currency={currency}/>
-      <CurrencySelector currency={currency} setCurrency={updateCurrency}/>
     </>
   )
 }
--- blog/src/Currency.jsx
+++ blog/src/Currency.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CoinBase from './CoinBase'
+import CurrencySelector from './CurrencySelector'
 
 function Currency() {
-  return (
-    <div>Currency</div>
-  )
+    const [currency, setCurrency] = useState("AUD")
+
+    // Abstract away setCurrency from CurrencySelector component, so it doesn't know what setCurrency is
+    const updateCurrency = (code) => {
+        // Validation of code
+        setCurrency(code)
+    }
+
+    return (
+        <>
+            <CoinBase currency={currency}/>
+            <CurrencySelector currency={currency} setCurrency={updateCurrency}/>
+        </>
+    )
 }
 
 export default Currency
